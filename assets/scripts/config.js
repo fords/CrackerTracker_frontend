2558c1ac02d6e89ccc19fad68347c5ce186769c9
fords <zxw6805@rit.edu>
2018-12-10 10:26:26 -0500

    update config api url

--- assets/scripts/config.js
+++ assets/scripts/config.js
@@ -2,7 +2,7 @@
 
 let apiUrl
 const apiUrls = {
-  production: 'https://zey.herokuapp.com/',
+  production: 'https://boiling-ridge-83567.herokuapp.com/',
   development: 'http://localhost:4741'
 }
 
